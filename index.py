c26f7709fb837815438d9611df0d12225bb9efb2
hkussie <hkussie@gmail.com>
2019-08-18 11:12:42 -0700

    Implement GET request to Spellcheck API

--- index.py
+++ index.py
@@ -8,8 +8,8 @@ def main(sentence):
 	url = "https://montanaflynn-spellcheck.p.rapidapi.com/check/"
 	querystring = {"text":"{}".format(sentence)}
 	headers = {
-    'x-rapidapi-host': "montanaflynn-spellcheck.p.rapidapi.com",
-    'x-rapidapi-key': "3b2d1eb02fmsh98b6d725c983d35p1b42afjsn01388dd70d1d"
+    	'x-rapidapi-host': "montanaflynn-spellcheck.p.rapidapi.com",
+    	'x-rapidapi-key': "3b2d1eb02fmsh98b6d725c983d35p1b42afjsn01388dd70d1d"
 	}
 	response = requests.request("GET", url, headers=headers, params=querystring)
 	click.echo(response.text)
