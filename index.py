c7c1ad93969e35839751b132ec7cbf726f59a495
hkussie <hkussie@gmail.com>
2019-08-18 11:08:14 -0700

    Implement GET request to Spellcheck API

--- index.py
+++ index.py
@@ -3,8 +3,16 @@ import requests
 click.disable_unicode_literals_warning = True
 
 @click.command()
-def main(): 
-	click.echo("This is a CLI built with Click ✨")
+@click.argument('sentence')
+def main(sentence): 
+	url = "https://montanaflynn-spellcheck.p.rapidapi.com/check/"
+	querystring = {"text":"{}".format(sentence)}
+	headers = {
+    'x-rapidapi-host': "montanaflynn-spellcheck.p.rapidapi.com",
+    'x-rapidapi-key': "3b2d1eb02fmsh98b6d725c983d35p1b42afjsn01388dd70d1d"
+	}
+	response = requests.request("GET", url, headers=headers, params=querystring)
+	click.echo(response.text)
 
 if __name__ == "__main__":
 	main()
